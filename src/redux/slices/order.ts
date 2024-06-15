d9d91dc0f3f15de2e1a9a7aabd3257cfea2c613c
Stanislav Efimtsev <evendot@yandex.ru>
2024-06-15 13:07:27 +0300

    Order file cleaning

--- src/redux/slices/order.ts
+++ src/redux/slices/order.ts
@@ -3,38 +3,23 @@ import instance from "../../services/axios_helper";
 import getToken from "../../services/getToken";
 
 export const fetchOrders = createAsyncThunk("orders/fetchOrders", async () => {
-  // const cookies = new Cookies();
-  // const token = cookies.get("token");
   const token = getToken();
   const { data } = await instance.get("/orders", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return data;
 });
-// export const fetchOneOrder = createAsyncThunk(
-//   "orders/fetchOneOrder",
-//   async (id: string) => {
-//     const token = getToken();
-//     const { data } = await instance.get("/orders/" + id, {
-//       headers: { Authorization: `Bearer ${token}` },
-//     });
-//     return data;
-//   }
-// );
-// export const fetchNewOrder = createAsyncThunk(
-//   "orders/fetchNewOrder",
-//   async (params: any) => {
-//     const { data } = await instance.post("/orders", params);
-//     return data;
-//   }
-// );
-export const fetchMyOrders = createAsyncThunk("orders/fetchMyOrders", async (userId) => {
-  const token = getToken();
-  const { data } = await instance.get("/orders/user/" + userId, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return data;
-});
+
+export const fetchMyOrders = createAsyncThunk(
+  "orders/fetchMyOrders",
+  async (userId) => {
+    const token = getToken();
+    const { data } = await instance.get("/orders/user/" + userId, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return data;
+  }
+);
 const initialState = {
   orders: {
     items: [],
